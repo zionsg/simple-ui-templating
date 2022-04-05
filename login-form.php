2dbad0bc4dd2acd856cacc0954e4a27cf63e98d5
Zion Ng <zion@intzone.com>
2022-04-05 10:25:04 +0800

    Add dropdown with optgroup

--- login-form.php
+++ login-form.php
@@ -12,8 +12,8 @@
     <form method="POST" action="page-dashboard.php">
       <div class="input-group">
         <span class="input-group-text font-monospace">Username</span>
-        <input name="username" required autofocus type="text" class="form-control"
-          placeholder="Enter your username here" />
+        <input name="username" required autofocus type="text"
+          class="form-control" placeholder="Enter your username here" />
       </div>
 
       <div class="input-group has-validation mb-3">
@@ -22,8 +22,35 @@
           placeholder="Enter your password here" />
       </div>
 
-      <input type="hidden" name="_csrf" value="<?= $csrfToken ?>" />
-      <button type="submit" class="btn btn-primary w-100">Login</button>
+      <div>
+        <select name="favorite_food" class="form-select">
+          <option value="" selected>Please select your favorite food</option>
+          <optgroup label="Fruits">
+            <option value="apple">Apple</option>
+            <option value="banana">Banana</option>
+            <option value="cranberries">Cranberries</option>
+          </optgroup>
+          <optgroup label="Grain">
+            <option value="amaranth">Amaranth</option>
+            <option value="broccoli">Buckwheat</option>
+            <option value="corn">Corn</option>
+          </optgroup>
+          <optgroup label="Vegetables">
+            <option value="asparagus">Asparagus</option>
+            <option value="broccoli">Broccoli</option>
+            <option value="carrot">Carrot</option>
+          </optgroup>
+          <option value="-1">Others</option>
+        </select>
+
+        <input name="other_favorite_food" type="text" class="form-control mt-2 d-none"
+          placeholder="Enter other food here" />
+      </div>
+
+      <div class="mt-3">
+        <input type="hidden" name="_csrf" value="<?= $csrfToken ?>" />
+        <button type="submit" class="btn btn-primary w-100">Login</button>
+      </div>
     </form>
   </div>
 </section>
@@ -42,5 +69,15 @@
             },
             1000
         );
+
+        // Toggle display of other field based on value in dropdown
+        let dropdown = view.querySelector('select[name="favorite_food"]');
+        let otherField = view.querySelector('input[name="other_favorite_food"]');
+        dropdown.addEventListener('change', (event) => {
+            let isOthersSelected = (-1 === parseInt(event.target.value));
+            otherField.required = isOthersSelected;
+            otherField.classList.toggle('d-none', isOthersSelected ? false : true); // remove d-none if Others chosen
+        });
+        dropdown.dispatchEvent(new Event('change')); // trigger on page load
     })();
 </script>
